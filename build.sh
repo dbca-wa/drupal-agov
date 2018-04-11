ebd9f7c99f2896bea25611d90554ac3574544f69
Adon Metcalfe <adonm@fastmail.fm>
2018-04-11 16:45:50 +0800

    init

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+APP_DIR=drupal
+drush dl drupal-8.x.x
+rm -rf $APP_DIR; mv -v drupal-8* $APP_DIR
+pushd $APP_DIR
+drush dl admin_toolbar agov_base agov_whitlam better_normalizers ctools default_content ds embed 
+drush dl entity entity_browser entity_embed fences inline_entity_form link_attributes linkit linky 
+drush dl media_entity media_entity_browser media_entity_image metatag page_manager panels password_policy 
+drush dl pathauto pnx_media pnx_media_embed scheduled_updates simple_sitemap token twitter_block 
+drush dl video_embed_field workbench_moderation
+popd
+cp -v src/settings.php $APP_DIR/sites/default/settings.php
+
