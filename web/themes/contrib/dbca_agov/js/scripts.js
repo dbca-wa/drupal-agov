ac5dd9e021b6f63df1445dea7046e7fc3e8a5fc7
Solanki <nitins@corporateict.domain@nitins-mate-dev.lan.fyi>
2019-05-24 10:58:45 +0800

    added modififed files

--- web/themes/contrib/dbca_agov/js/scripts.js
+++ web/themes/contrib/dbca_agov/js/scripts.js
@@ -1,5 +1,9 @@
 jQuery(document).ready(function ($) {
 
+
+ // $("#mm-1 .mm-navbar").append("<a href='#' class='mm-navbar__closer' style=''>Menu</a>");
+
+  // Footer scroll to top function
   $(window).scroll(function () {
     if ($(this).scrollTop() > 100) {
       $('.scroll-to-top').fadeIn();
@@ -8,7 +12,14 @@ jQuery(document).ready(function ($) {
     }
   });
 
+  $('.scroll-to-top').click(function () {
+    $("html, body").animate({
+      scrollTop: 0
+    }, 100);
+      return false;
+  });
 
+  // Focus on top search
    $('#search-block-form input').on('focusin', function () {
           $(this).parents('form').addClass('active');
         });
@@ -16,15 +27,19 @@ jQuery(document).ready(function ($) {
           $(this).parents('form').removeClass('active');
    });
 
-
-
-  $('.scroll-to-top').click(function () {
-    $("html, body").animate({
-      scrollTop: 0
-    }, 100);
-      return false;
+  // Display Author read more
+  $('#showAuthor').click(function () {
+  $('#AuthorInfo').toggle();
+   return false;
   });
-  
+
+
+  // Side menu
+  $('.mm-navbar .mm-navbar__closer').click(function () {
+    e.preventDefault();
+    $('html').removeClass('mm-wrapper_opened mm-wrapper_blocking mm-wrapper_background mm-wrapper_opening');
+    $('#off-canvas').removeClass('mm-menu_opened');
+  }); 
 
   $(window).resize(function() {
     var width = $(document).width();
@@ -36,12 +51,80 @@ jQuery(document).ready(function ($) {
     }
    });
 
+  // Prevent submission of blank search
+/*
+$('.header_search form').submit(function () {
+    // Get the Login Name value and trim it
+    var searchWord = $.trim($('input.form-search').val());
+    // Check if empty of not
+    if (searchWord === '') {
+        return false;
+    }
+});
+*/
+
+
+/* Table of contents */
+var ToC =
+    "<h2>On this page:</h2>" +
+    "<ul>";
+
+var newLine, el, title, link;
+$(".accordionList h1").each(function() {
+  el = $(this);
+  title = el.text();
+  link = "#" + el.attr("id");
+  newLine =
+    "<li>" +
+      "<a href='" + link + "'>" +
+        title +
+      "</a>" +
+    "</li>";
+  ToC += newLine;
+});
+ToC +=
+   "</ul>";
+$(".tableofcontents").prepend(ToC);
+
+
+// Smooth scroll 
+$('a[href^="#"]').bind('click.smoothscroll',function (e) {
+    e.preventDefault();
+    var target = this.hash,
+        $target = $(target);
+
+    $('html, body').stop().animate( {
+      'scrollTop': $target.offset().top-50
+    }, 900, 'swing', function () {
+      window.location.hash = target;
+    } );
+  } );
+
 
 });  
 
+  //Legacy opening URL's
 function popitup(url) {
-newwindow=window.open(url,'name','height=800,width=1050');
-if (window.focus) {newwindow.focus()}
-return false;
+    newwindow = window.open(url, 'name', 'height=800,width=1050');
+    if (window.focus) {
+        newwindow.focus()
+    }
+    return false;
 }
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
 
